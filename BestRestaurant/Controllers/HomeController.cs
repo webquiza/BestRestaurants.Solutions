0dbaebe93eced73ba8405d1d62713f129853d76b
Carlos Urquiza <webquiza@gmail.com>
2021-10-04 20:34:59 -0700

    update practice project

--- BestRestaurant/Controllers/HomeController.cs
+++ BestRestaurant/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace ToDoList.Controllers
+namespace BestRestaurant.Controllers
 {
     public class HomeController : Controller
     {
--- BestRestaurant/Startup.cs
+++ BestRestaurant/Startup.cs
@@ -4,9 +4,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ToDoList.Models;
+using BestRestaurant.Models;
 
-namespace ToDoList
+namespace BestRestaurant
 {
   public class Startup
   {
@@ -25,7 +25,7 @@ namespace ToDoList
       services.AddMvc();
 
       services.AddEntityFrameworkMySql()
-        .AddDbContext<ToDoListContext>(options => options
+        .AddDbContext<BestRestaurantContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
     }
 
